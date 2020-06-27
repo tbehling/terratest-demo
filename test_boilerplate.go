8aa50773a74cc2951e6ac1c5f719d00327c256e6
Ted Behling <tbehling@github.com>
2020-06-27 15:39:26 -0400

    Hook into Go's testing library from regular code

---
+++ test_boilerplate.go
@@ -0,0 +1,34 @@
+//+build mage
+
+package main
+
+import (
+	"io"
+	"testing"
+	"regexp"
+	"errors"
+)
+
+func RunGoTests(tests []testing.InternalTest) error {
+	m := testing.MainStart(&nullTestDeps{}, tests, nil, nil)
+	if x := m.Run(); x > 0 {
+		return errors.New("testing.MainStart returned non-zero")
+	}
+	return nil
+}
+
+// nullTestDeps implements testing.testDeps to stub dependencies for testing.MainStart()
+// 
+// See https://stackoverflow.com/questions/59064256/is-it-possible-to-call-a-test-func-from-another-file-to-start-the-testing
+//     https://golang.org/src/testing/testing.go?s=38015:38129#L1146
+//     https://golang.org/src/testing/internal/testdeps/deps.go
+type nullTestDeps struct{}
+
+func (d *nullTestDeps) MatchString(pat, str string) (bool, error)         { return regexp.MatchString(pat, str) }
+func (d *nullTestDeps) StartCPUProfile(_ io.Writer) error                 { return nil }
+func (d *nullTestDeps) StopCPUProfile()                                   {}
+func (d *nullTestDeps) WriteHeapProfile(_ io.Writer) error                { return nil }
+func (d *nullTestDeps) WriteProfileTo(_ string, _ io.Writer, _ int) error { return nil }
+func (d *nullTestDeps) ImportPath() string                                { return "" }
+func (d *nullTestDeps) StartTestLog(io.Writer) { return }
+func (d *nullTestDeps) StopTestLog() error { return nil}
