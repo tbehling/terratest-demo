634faf5affcbe9344c86f86aaef99b0185386410
Ted Behling <tbehling@github.com>
2020-06-27 16:59:14 -0400

    Add demo Terratest from example
    From https://terratest.gruntwork.io/docs/getting-started/quick-start/

--- magefile.go
+++ magefile.go
@@ -28,6 +28,19 @@ func Terratest() error {
 }
 
 func TestRunTerratest(t *testing.T) {
-	t.Error("placeholder failure")
+	terraformOptions := &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: ".",
+	}
+
+	// Clean up resources with "terraform destroy" at the end of the test.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables and check they have the expected values.
+	output := terraform.Output(t, terraformOptions, "hello_world")
+	assert.Equal(t, "Hello, World!", output)
 }
 
